1e602e464f23512518163512f13c719a810a3a67
baoloc008 <baoloc008@gmail.com>
2020-06-05 17:34:48 +0700

    init server

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	r := http.NewServeMux()
+
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	s := http.Server{
+		Addr:":9620",
+		Handler:r,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
